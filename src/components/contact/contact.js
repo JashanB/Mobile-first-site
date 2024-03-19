7e14c552440fd5136407f5ec3db7aa64d3c515cf
Jashan Brar <jashan_brar50@hotmail.com>
2024-03-19 16:20:40 -0400

    cleanup file

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -21,11 +21,7 @@ export default (props) => {
       return true;
     }
   }
-  // function submitForm(firstName, lastName, email, message) {
-  //   if (firstName.length > 0 && lastName.length > 0 && verifyEmail(email) && message.length > 0) {
-  //     setSubmitted(true);
-  //   }
-  // }
+  
   function submitForm(name, email, message) {
     if (!name || name.length === 0) {
       setNameError(true);
@@ -51,88 +47,54 @@ export default (props) => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'template_ebjzkv9', {name: name, emai: email, message: message}, '-gZwc_YCDpSfGPa3n')
+    emailjs.sendForm('YOUR_SERVICE_ID', 'template_ebjzkv9', { name: name, emai: email, message: message }, '-gZwc_YCDpSfGPa3n')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
   };
 
   //if submitted and values correct, switch to display Thank you   
-    return (
-      <div className="content_div_type3">
-        <div className='contact-subheader'>
-          {submitted ? <h2>Thank you!</h2> : <h2>Contact Me</h2>}
+  return (
+    <div className="content_div_type3">
+      <div className='contact-subheader'>
+        {submitted ? <h2>Thank you!</h2> : <h2>Contact Me</h2>}
+      </div>
+      <div className={submitted ? 'text_input_box_hidden' : 'text_input_box'}>
+        <div className="box_title">
+          <h3>Name*</h3>
+          {nameError && <h3 className='error_text'>Field Required</h3>}
         </div>
-        <div className={submitted ? 'text_input_box_hidden' : 'text_input_box'}>
-          <div className="box_title">
-            <h3>Name*</h3>
-            {nameError && <h3 className='error_text'>Field Required</h3>}
-          </div>
-          <div className="input_div_sub">
-            {/* <input type="text" id="firstName" name="firstName" onChange={(event) => setFirstName(event.target.value)}></input> */}
-            <input type="text" id="firstName" name="firstName" onChange={(event) => setName(event.target.value)}></input>
-          </div>
-        </div>
-        {/* <div className='text_input_box'>
-          <div className="box_title">
-            <h3>Name*</h3>
-          </div>
-          <div className="dual_input_div">
-            <div className="input_div_sub" id="dual_input_div1">
-              <input type="text" id="firstName" name="firstName" onChange={(event) => setFirstName(event.target.value)}></input>
-  
-              <div className="box_title">
-                <h3>First Name</h3>
-              </div>
-            </div>
-            
-            <div className="input_div_sub">
-              <input type="text" id="lastName" name="lastName" onChange={(event) => setLastname(event.target.value)}></input>
-              <div className="box_title">
-                <h3>Last Name</h3>
-              </div> 
-            </div> 
-          </div>
-        </div> */}
-        <div className={submitted ? 'text_input_box_hidden' : 'text_input_box'}>
-          <div className="box_title">
-            <h3>Email*</h3>
-            {emailError && <h3 className='error_text'>Please use a valid email</h3>}
-          </div>
-          <div className="input_div_sub">
-            <input type="text" id="email" name="email" onChange={(event) => setEmail(event.target.value)}></input>
-          </div>
-        </div>
-        {/* <div className='text_input_box'>
-          <div className="box_title">
-            <h3>Subject*</h3>
-          </div>
-          <div className="input_div_sub">
-            <input type="text" id="subject" name="subject" onChange={(event) => setSubject(event.target.value)}></input>
-          </div>
-        </div>  */}
-        <div className={submitted ? 'text_input_box_hidden' : 'text_input_box'}>
-          <div className="box_title">
-            <h3>Message*</h3>
-            {msgError && <h3 className='error_text'>Field Required</h3>}
-          </div>
-          <div className="input_div_sub-lg">
-            <textarea type="text" id="message" name="message" onChange={(event) => setMessage(event.target.value)}></textarea>
-          </div>
-        </div>
-        {/* <div className='submit_button_div'>
-              <button onClick={() => submitForm(firstName, lastName, email, message)}>Send</button>
-            </div> */}
-        <div className={submitted ? 'send_button_div_left_hidden' : 'send_button_div_left'}>
-          {/* <button className='book_button' onClick={() => submitForm(firstName, lastName, email, message)}>Send</button> */}
-          <button className='book_button' onClick={() => submitForm(name, email, message)}>Send</button>
-
+        <div className="input_div_sub">
+          {/* <input type="text" id="firstName" name="firstName" onChange={(event) => setFirstName(event.target.value)}></input> */}
+          <input type="text" id="firstName" name="firstName" onChange={(event) => setName(event.target.value)}></input>
         </div>
       </div>
-    )
- 
-  
+      <div className={submitted ? 'text_input_box_hidden' : 'text_input_box'}>
+        <div className="box_title">
+          <h3>Email*</h3>
+          {emailError && <h3 className='error_text'>Please use a valid email</h3>}
+        </div>
+        <div className="input_div_sub">
+          <input type="text" id="email" name="email" onChange={(event) => setEmail(event.target.value)}></input>
+        </div>
+      </div>
+      <div className={submitted ? 'text_input_box_hidden' : 'text_input_box'}>
+        <div className="box_title">
+          <h3>Message*</h3>
+          {msgError && <h3 className='error_text'>Field Required</h3>}
+        </div>
+        <div className="input_div_sub-lg">
+          <textarea type="text" id="message" name="message" onChange={(event) => setMessage(event.target.value)}></textarea>
+        </div>
+      </div>
+      <div className={submitted ? 'send_button_div_left_hidden' : 'send_button_div_left'}>
+        <button className='book_button' onClick={() => submitForm(name, email, message)}>Send</button>
+      </div>
+    </div>
+  )
+
+
 
 }
